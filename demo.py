6771b1137b35ff9144960d12e41d6a4e84c09ce1
Vidyananda Kashyap A <vidyanandakashyap@gmail.com>
2023-09-24 23:13:09 +0530

    Linear Search

--- demo.py
+++ demo.py
@@ -1,2 +1,16 @@
-print("Vidyananda")
-print("Kashyap A")
+pos=-1
+def search(list):
+    i=0
+    while i<len(list):
+        if list[i]==n:
+            globals()['pos']=i
+            return True
+        i+=1
+    return False
+
+list=[1,2,3,4,5,6,11,8,9,10]
+n=12
+if search(list):
+    print("Found at ",pos+1)
+else:
+    print("NOt Found")
